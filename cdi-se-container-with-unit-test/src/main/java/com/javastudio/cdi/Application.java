798c3c022f87dd73bff072623351e6cb155dfb21
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-02-16 19:23:34 +0330

    cdi

---
+++ cdi-se-container-with-unit-test/src/main/java/com/javastudio/cdi/Application.java
@@ -0,0 +1,35 @@
+package com.javastudio.cdi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.concurrent.ExecutionException;
+
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SeContainer container = null;
+        try {
+            SeContainerInitializer containerInit = SeContainerInitializer.newInstance();
+            container = containerInit.initialize();
+
+            // Fire synchronous event that triggers the code in App class.
+            Application application = container.select(Application.class).get();
+
+            application.run();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            if (container != null)
+                container.close();
+        }
+    }
+
+    void run() {
+        LOGGER.info("Cdi se container started!");
+    }
+}
